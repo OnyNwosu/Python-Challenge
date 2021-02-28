91cc016347b49056f770e66bc7d27a7644f65440
OnyNwosu <onuwa.nwosu@gmail.com>
2021-02-27 21:00:28 -0600

    added print statements

--- PyBank/bank.py
+++ PyBank/bank.py
@@ -2,7 +2,7 @@ import os
 import csv
 
 file = os.path.join("..", "Bank CSV File", "budget_data.csv")
-with open("budget_data.csv", "b") as csvfile:
+with open("budget_data.csv", "r") as csvfile:
     csvreader = csv.reader(csvfile, delimiter = ",") 
     header = next(csvreader)
     Month_Count = []
@@ -14,3 +14,17 @@ with open("budget_data.csv", "b") as csvfile:
         Profit.append(row[1])
     for i in range(len(Profit)-1):
         Change_In_Profit.append(Profit[i+1]-Profit[i])
+
+increase = max(Change_In_Profit)
+decrease = min(Change_In_Profit)
+
+Month_Increase = Change_In_Profit.index(max(Change_In_Profit))+1
+Month_Decrease = Change_In_Profit.index(min(Change_In_Profit))+1
+
+print("Financial Analysis")
+print("__________________________")
+print(f"Total Months:{len(Month_Count)}")
+print(f"Total: ${sum(Profit)}")
+print(f"Average Change: {sum(Change_In_Profit)/len(Change_In_Profit),2)}")
+print(f"Greatest Increase in Profits: {Month_Count[Month_Increase]}(${(str(increase))})")
+print(fGreatest Decrease in Profits: {Month_Count[Month_Decrease]}(${(str(decrease))})" )
