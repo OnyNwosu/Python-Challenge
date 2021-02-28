14e4908f31fb0009b47788fa71d9bc60cf3aa4cd
OnyNwosu <onuwa.nwosu@gmail.com>
2021-02-27 23:56:14 -0600

    last changes

--- PyPoll/poll.py
+++ PyPoll/poll.py
@@ -3,43 +3,52 @@ import csv
 
 Election_Data = os.path.join('..', 'Resources', 'election_data.csv')
 Candidates = []
+Candidate_List = []
 Number_Votes = []
 Percent_Votes = []
 Total_Votes = 0
 
-with open(election_data) as csvfile
+csvpath = os.path.join('Resources', 'election_data')
+with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter = ",")
     cvsheader = next(csvreader)
     for row in csvreader
     Total_Votes +=1
 
     if row[2] not in candidates:
-            candidates.append(row[2])
-            index = candidates.index(row[2])
-            num_votes.append(1)
+            Candidate_List.append(row[2])
+            index = Candidates.index(row[2])
+            Number_Votes.append(1)
     else:
         index = candidates.index(row[2])
-        num_votes[index] += 1
+        Number_Votes[index] += 1
 
-    for votes in num_votes:
+    for Total_Votes in Number_Votes:
         percentage = (votes/total_votes) * 100
         percentage = round(percentage)
-        percentage = "%.3f%%" % percentage
-        percent_votes.append(percentage)
 
-winner = max(num_votes)
-index = num_votes.index(winner)
-winning_candidate = candidates[index]
+Winner = max(Number_Votes)
+index = Number_Votes.index(winner)
+Winning_Candidate = Candidates[index]
 
 print("Election Results")
 print("--------------------------")
 print(f"Total Votes: {str(total_votes)}")
 print("--------------------------")
-for i in range(len(candidates)):
-    print(f"{candidates[i]}: {str(percent_votes[i])} ({str(num_votes[i])})")
+for i in range(len(Candidates)):
+    print(f"{Candidates[i]}: {str(Percent_Votes[i])} ({str(Number_Votes[i])})")
 print("--------------------------")
-print(f"Winner: {winning_candidate}")
+print(f"Winner: {Winning_Candidate}")
 print("--------------------------")
 
 output = output.txt
-with open(output, "w") as new:
+with open(output, "w") as txt_file:
+    txt_file.write("Election Results")
+    txt_file.write("--------------------------")
+    txt_file.write(f"Total Votes: {str(total_votes)}")
+    txt_file.write("--------------------------")
+    txt_file.write(f"{Candidates[i]}: {str(Percent_Votes[i])} ({str(Number_Votes[i])})")
+    txt_file.write("--------------------------")
+    txt_file.write("--------------------------")
+    txt_file.write(f"Winner: {Winning_Candidate}")
+    txt_file.write("--------------------------")
